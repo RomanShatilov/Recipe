206719b7a55b17c609b4cfd1955791cc54de45cb
RomanKopfjager <roman.thedeveloper@gmail.com>
2020-06-21 19:34:48 +0300

    v1

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+  // the same kind of magic happens here!
+});
